f050fb0a75957e8c645848e6cd52c18248010eaf
xiaoba <xiaobayuwei@gmail.com>
2012-12-05 22:57:13 +0800

    demo

---
+++ server.py
@@ -0,0 +1,44 @@
+#1/usr/bin/env python
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+
+define('port', default=8000, help='run on the given port', type=int)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        f = open('assets/demo.html')
+        content = f.read()
+        f.close()
+        self.finish(content)
+
+    def post(self):
+        print self.request.arguments
+
+        key = self.get_argument('key')
+        if not key:
+            self.finish('{"stat":"fail", "msg":"missing argument"}')
+
+        print 'got a post argument key as value %s' % key
+        self.write('{"stat":"ok", "msg": "got arg key" }')
+
+
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r'/', MainHandler),
+        (r'/src/(.*)', tornado.web.StaticFileHandler, {'path': 'src/'}),
+        (r'/assets/(.*)', tornado.web.StaticFileHandler, {'path': 'assets/'}),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    print 'Starting server at: 127.0.0.1:%s' % options.port
+    main()
